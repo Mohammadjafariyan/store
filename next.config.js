a2c2210d4811865c3be524af9c89956bed2e4eef
Mohammad Jafariyan <mohammad.jafariyan7@gmail.com>
2023-02-10 03:28:41 +0330

    update

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const nextConfig = {
+  // Output the static files to somewhere more obvious
+  distDir: 'build',
+  experimental: {
+    // This is experimental but can
+    // be enabled to allow parallel threads
+    // with nextjs automatic static generation
+    workerThreads: false,
+    cpus: 4
+  }
+}
+
+module.exports = nextConfig
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const next = require("next");
 
 //const port = parseInt(process.env.PORT, 10) || 4000;
 const port =  4000;
-const dev = process.env.NODE_ENV !== "production";
+const dev = true ;//process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -16,6 +16,6 @@ app.prepare().then(() => {
 
   server.listen(port, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(`> Ready on http://localhost:${port} ${dev}`);
   });
 });
